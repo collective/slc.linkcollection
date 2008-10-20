f25a32923c596949ef28134065793b9f702c333a
pilz <pilz@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2008-10-20 08:36:33 +0000

    added styles, fixed bug with display, changed url to be stored in id

--- slc/linkcollection/browser/viewlets.py
+++ slc/linkcollection/browser/viewlets.py
@@ -4,12 +4,21 @@ from slc.linkcollection.interfaces import ILinkList
 from types import *
 from Products.CMFCore.utils import getToolByName
 from zope.app.component.hooks import getSite
+from Products.ATContentTypes.interface.document import IATDocument
 
 class LinkBoxViewlet(common.ViewletBase):
 
     render = ViewPageTemplateFile('linkbox.pt')
     
+    def raw(self):
+        if not IATDocument.providedBy(self.context):
+            return []
+        urls = ILinkList(self.context).urls
+        return urls
+        
     def links(self):
+        if not IATDocument.providedBy(self.context):
+            return []
         urls = ILinkList(self.context).urls
         if not urls:
             return []
@@ -18,9 +27,11 @@ class LinkBoxViewlet(common.ViewletBase):
         if type(urls) not in (ListType, TupleType):
             urls = [urls]
         for url in urls:
-            ob = portal.restrictedTraverse(url[1:], None)
+            if url.startswith('/'):
+                url = url[1:]
+            ob = portal.restrictedTraverse(url, None)
             
             if ob is not None:
-                maps.append(dict(title=ob.Title(), url=url[1:]))
+                maps.append(dict(title=ob.Title(), url=url))
                 
         return maps
