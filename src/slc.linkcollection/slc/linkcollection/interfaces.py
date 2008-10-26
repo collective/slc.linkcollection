c6d39d09315704a2d6d1d545711e6df8dea80ad4
pilz <pilz@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2008-10-26 13:07:18 +0000

    selector now shows the contents of the current folder ba default

--- src/slc.linkcollection/slc/linkcollection/interfaces.py
+++ src/slc.linkcollection/slc/linkcollection/interfaces.py
@@ -1,19 +1,32 @@
+from Acquisition import aq_inner
 from zope.interface import Interface
 from zope import schema
-from plone.app.vocabularies.catalog import SearchableTextSourceBinder
+from plone.app.vocabularies.catalog import SearchableTextSourceBinder, SearchableTextSource
 from Products.ATContentTypes.interface import IATDocument
+from zope.app.component.hooks import getSite
 
 class ILinkCollectionLayer(Interface):
     """A layer specific to LinkCollection
     """
     
+class LocalSearchableTextSourceBinder(SearchableTextSourceBinder):
+    """ make the binder search in the local folder first """
+
+    def __call__(self, context):
+        site = getSite()
+        portal_url = site.portal_url
+        current_path = '/'+'/'.join(portal_url.getRelativeContentPath(site.REQUEST.PARENTS[1]))
+        self.default_query = 'path:%s' % current_path
+        return SearchableTextSource(context, base_query=self.query.copy(),
+                                    default_query=self.default_query)    
+                                    
 class ILinkList(Interface):
-    urls = schema.List(title=u"Referenced Documents",#
-                       description=u"Find the document which provides the content",
+    urls = schema.List(title=u"Referenced Documents",
+                       description=u"Search and select the documents you want to add to your linklist. The first box contains your current selection. Below it you can do a fulltext search for documents. Below the search are the search results you can pic from. Initially they show the contents of the current folder.",
                        required=True,
                        value_type=schema.Choice(title=u"Referenced Documents",
-                                    description=u"Find the document which provides the content",
-                                    source=SearchableTextSourceBinder({'object_provides' : IATDocument.__identifier__},
-                                                                        default_query='path:'))
-                       )
-
+                                    description=u"!Search and select the documents you want to add to your linklist. The first box contains your current selection. Below it you can do a fulltext search for documents. Below the search are the search results you can pic from. Initially they show the contents of the current folder.",
+                                    source=LocalSearchableTextSourceBinder({'object_provides' : IATDocument.__identifier__})
+                                    )
+                        )
+    
