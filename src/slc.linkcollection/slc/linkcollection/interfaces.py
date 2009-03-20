3c9b865721e85b9c7f0c279027973da994f9e4f7
pilz <pilz@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2009-03-20 11:05:58 +0000

    Fixed a but where getSite() didn't return the site, but instead the form, causing a REQUEST attribute error.

--- src/slc.linkcollection/slc/linkcollection/interfaces.py
+++ src/slc.linkcollection/slc/linkcollection/interfaces.py
@@ -1,9 +1,13 @@
 from Acquisition import aq_inner
-from zope.interface import Interface
+
 from zope import schema
-from plone.app.vocabularies.catalog import SearchableTextSourceBinder, SearchableTextSource
-from Products.ATContentTypes.interface import IATDocument
 from zope.app.component.hooks import getSite
+from zope.interface import Interface
+
+from plone.app.vocabularies.catalog import SearchableTextSourceBinder, SearchableTextSource
+
+from Products.ATContentTypes.interface import IATDocument
+from Products.CMFCore.utils import getToolByName
 
 class ILinkCollectionLayer(Interface):
     """A layer specific to LinkCollection
@@ -13,20 +17,27 @@ class LocalSearchableTextSourceBinder(SearchableTextSourceBinder):
     """ make the binder search in the local folder first """
 
     def __call__(self, context):
-        site = getSite()
-        portal_url = site.portal_url
-        current_path = '/'+'/'.join(portal_url.getRelativeContentPath(site.REQUEST.PARENTS[1]))
+        portal_url = getToolByName(context, 'portal_url', None)
+        if IATDocument.implementedBy(context.__class__):
+            parent = context.REQUEST.PARENTS[1]
+        else:
+            parent = getSite()
+        current_path = '/'+'/'.join(portal_url.getRelativeContentPath(parent))
         self.default_query = 'path:%s' % current_path
         return SearchableTextSource(context, base_query=self.query.copy(),
                                     default_query=self.default_query)    
                                     
 class ILinkList(Interface):
-    urls = schema.List(title=u"Referenced Documents",
-                       description=u"Search and select the documents you want to add to your linklist. The first box contains your current selection. Below it you can do a fulltext search for documents. Below the search are the search results you can pic from. Initially they show the contents of the current folder.",
-                       required=True,
-                       value_type=schema.Choice(title=u"Referenced Documents",
-                                    description=u"!Search and select the documents you want to add to your linklist. The first box contains your current selection. Below it you can do a fulltext search for documents. Below the search are the search results you can pic from. Initially they show the contents of the current folder.",
-                                    source=LocalSearchableTextSourceBinder({'object_provides' : IATDocument.__identifier__})
-                                    )
+    urls = schema.List(
+            title=u"Referenced Documents",
+            description=\
+u"""Search and select the documents you want to add to your linklist. The first box contains your current selection. Below it you can do a fulltext search for documents. Below the search are the search results you can pic from. Initially they show the contents of the current folder.""",
+            required=True,
+            value_type=schema.Choice(
+                        title=u"Add documents for referencing",
+                        source=LocalSearchableTextSourceBinder(
+                                    {'object_provides':IATDocument.__identifier__}
+                                )
                         )
+            )
     
