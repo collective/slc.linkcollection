0f948adc9c1055ac092510eb9502a35923384a08
pilz <pilz@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2009-06-08 22:00:39 +0000

    prepared release

--- setup.py
+++ setup.py
@@ -1,20 +1,49 @@
-from setuptools import setup, find_packages
+# -*- coding: utf-8 -*-
+"""
+This module contains the slc.linkcollection package
+"""
 import os
+from setuptools import setup, find_packages
 
-version = '0.1'
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+version = '1.1'
+
+long_description = (
+    read('README.txt')
+    + '\n' +
+    'Change history\n'
+    '**************\n'
+    + '\n' +
+    read('CHANGES.txt')
+    + '\n' +
+    'Detailed Documentation\n'
+    '**********************\n'
+    + '\n' +
+    read('slc', 'linkcollection', 'README.txt')
+    + '\n' +
+    'Contributors\n'
+    '************\n'
+    + '\n' +
+    read('CONTRIBUTORS.txt')
+    + '\n'
+    )
+
+tests_require=['zope.testing']
+
 
 setup(name='slc.linkcollection',
       version=version,
-      description="A viewlet displaying links to subdocuments and quickly loading their content via kss",
-      long_description=open("README.txt").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      description="A viewlet displaying links to subdocuments and quickly loading their content via jquery",
+      long_description=long_description,
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
         "Framework :: Plone",
         "Programming Language :: Python",
         "Topic :: Software Development :: Libraries :: Python Modules",
         ],
-      keywords='viewlet links kss subnaviation',
+      keywords='viewlet links jquery subnavigation',
       author='Syslab.com GmbH',
       author_email='info@syslab.com',
       url='http://svn.plone.org/svn/plone/plone.example',
@@ -28,7 +57,12 @@ setup(name='slc.linkcollection',
 	  'plone.browserlayer',
 	  
       ],
+      tests_require=tests_require,
+      extras_require=dict(tests=tests_require),
+      test_suite = 'slc.linkcollection.tests.test_docs.test_suite',
       entry_points="""
-      # -*- Entry points: -*-
+      # -*- entry_points -*-
       """,
+      paster_plugins = ["ZopeSkel"],
       )
+
