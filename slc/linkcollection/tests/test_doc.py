53a80f7d9c60c4a7ae1d8da98cc4037bf91c53ff
do3cc <do3cc@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2009-06-16 13:10:58 +0000

    Fixed tests

--- slc/linkcollection/tests/test_doc.py
+++ slc/linkcollection/tests/test_doc.py
@@ -4,26 +4,16 @@ import unittest
 from zope.testing import doctest
 from zope.component import testing
 from Products.PloneTestCase import PloneTestCase
+from Products.PloneTestCase.layer import PloneSite
 from Testing.ZopeTestCase import FunctionalDocFileSuite as Suite
+from Testing import ZopeTestCase as ztc
 from Products.PloneTestCase.layer import onsetup
 from Globals import package_home
 from slc.linkcollection import GLOBALS
-from z3c.form import testing
 
 
-class LinkCollectionFunctionalTestCase(PloneTestCase.FunctionalTestCase):
-   pass
-
-@onsetup
-def setup_linkcollection():
-    from Products.Five import zcml
-    from Products.Five import fiveconfigure
-    fiveconfigure.debug_mode = True
-    import slc.linkcollection
-    zcml.load_config('configure.zcml', slc.linkcollection)
-    fiveconfigure.debug_mode = False
-
-setup_linkcollection()
+class LinkCollectionFunctionalTestCase(PloneTestCase.PloneTestCase):
+    pass
 
 OPTIONFLAGS = (doctest.REPORT_ONLY_FIRST_FAILURE |
                doctest.ELLIPSIS |
@@ -31,19 +21,16 @@ OPTIONFLAGS = (doctest.REPORT_ONLY_FIRST_FAILURE |
 
 PloneTestCase.setupPloneSite()
 
-
-
 def test_suite():
     home = package_home(GLOBALS)
-    print home+'/README.txt',
+    print home + '/README.txt',
     return unittest.TestSuite([
 
         Suite(
            'README.txt',
            optionflags=OPTIONFLAGS,
            package='slc.linkcollection',
-           setUp=testing.setUp, tearDown=testing.tearDown,
            test_class=LinkCollectionFunctionalTestCase
            ),
-           
+
         ])
