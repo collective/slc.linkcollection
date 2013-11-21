c2c5a31492a5702848b17cc89d8394e32f663931
Cillian de Roiste <deroiste@syslab.com>
2013-11-21 15:01:01 +0000

    r252696 Use jq instead of jQuery throughout, this helps with compatibility and shouldn't do any harm
    This fixes a jQuery error triggered when using
    wildcard.foldercontents, since that includes a newer version of jQuery.

--- slc/linkcollection/browser/linkcollection.js
+++ slc/linkcollection/browser/linkcollection.js
@@ -43,25 +43,25 @@ LinkCollection.render_doc = function render_doc(node, uid) {
 }
 
 /* New style link collection, all content in one document, usinj jQuery */
-jQuery(function() {
-    var elems = jQuery("h2.linkcollection");
-    jQuery("h2.linkcollection").nextAll().andSelf().wrapAll('<div id="tabs" />');
-    jQuery("<ul id='slc-linkcollection-list' class='navigationLinkBox'></ul>").prependTo("#tabs");
+jq(function() {
+    var elems = jq("h2.linkcollection");
+    jq("h2.linkcollection").nextAll().andSelf().wrapAll('<div id="tabs" />');
+    jq("<ul id='slc-linkcollection-list' class='navigationLinkBox'></ul>").prependTo("#tabs");
     for (var i=0;i<elems.length;i++)
     {
-        var listel = jQuery("<li>")[0];
-        var linkel = jQuery("<a href='#tabs-" + i + "' class='linkcollectionAnchor'></a>)")[0];
-        jQuery(linkel).append(jQuery(elems[i]).text());
-        jQuery(listel).append(linkel);
-        jQuery("#tabs ul#slc-linkcollection-list").append(listel);
-        jQuery(elems[i]).nextUntil("h2.linkcollection").andSelf().wrapAll("<div class='paneContent' id='tabs-" + i + "' />");
+        var listel = jq("<li>")[0];
+        var linkel = jq("<a href='#tabs-" + i + "' class='linkcollectionAnchor'></a>)")[0];
+        jq(linkel).append(jq(elems[i]).text());
+        jq(listel).append(linkel);
+        jq("#tabs ul#slc-linkcollection-list").append(listel);
+        jq(elems[i]).nextUntil("h2.linkcollection").andSelf().wrapAll("<div class='paneContent' id='tabs-" + i + "' />");
     }
-    jQuery('#tabs div.paneContent').wrapAll("<div class='panes' />");
-    jQuery('#tabs').before('<a name="linkcollectionNavi"></a>');
-    var actual_url = jQuery('#actual-url').text();
-    jQuery('#tabs').after('<span class="linkToTopAnchor"><a href="' + actual_url + '#linkcollectionNavi" class="solitaryLink linkcollectionAnchor" i18n:translate="label_go_up">Go up</a></span>');
-    jQuery('#tabs').after('<div class="visualClear"></div>');
-    jQuery("ul.navigationLinkBox").tabs('div.panes > div.paneContent');
+    jq('#tabs div.paneContent').wrapAll("<div class='panes' />");
+    jq('#tabs').before('<a name="linkcollectionNavi"></a>');
+    var actual_url = jq('#actual-url').text();
+    jq('#tabs').after('<span class="linkToTopAnchor"><a href="' + actual_url + '#linkcollectionNavi" class="solitaryLink linkcollectionAnchor" i18n:translate="label_go_up">Go up</a></span>');
+    jq('#tabs').after('<div class="visualClear"></div>');
+    jq("ul.navigationLinkBox").tabs('div.panes > div.paneContent');
 });
 
 var jump=function(e)
@@ -69,12 +69,12 @@ var jump=function(e)
     //prevent the "normal" behaviour which would be a "hard" jump
     e.preventDefault();
     //Get the target
-    var target = jQuery("#tabs");
+    var target = jq("#tabs");
     //perform animated scrolling
-    jQuery('html,body').animate(
+    jq('html,body').animate(
     {
       //get top-position of target-element and set it as scroll target
-      scrollTop: jQuery(target).offset().top
+      scrollTop: jq(target).offset().top
       //scrolldelay: 2 seconds
     },500,function()
     {
@@ -82,9 +82,9 @@ var jump=function(e)
     });
 }
 
-jQuery(document).ready(function()
+jq(document).ready(function()
 {
-    jQuery('a[href*=#].linkcollectionAnchor').bind("click", jump);
+    jq('a[href*=#].linkcollectionAnchor').bind("click", jump);
     return false;
 });
 
